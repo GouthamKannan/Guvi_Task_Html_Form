effd632be40daad11dc84d9bce4d0efbfefcc1df
GouthamKannan <54542723+GouthamKannan@users.noreply.github.com>
2021-07-09 17:52:53 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+function formsubmit(){
+    let form = document.querySelector("form")
+    let formData = new FormData(form);
+    let firstName = formData.get("firstName");
+    let lastName = formData.get("lastName");
+    let inputAddress1 = formData.get("inputAddress1");
+    let inputAddress2 = formData.get("inputAddress2");
+    let pincode = formData.get("pincode");
+    let gender = formData.get("gender");
+    let food = formData.getAll("food");
+    let state = formData.get("state");
+    let country = formData.get("country");
+    if(food.length<2){
+        alert("must choose at least 2 out of 5 choices of food");
+    }
+    else{
+        let tbody = document.getElementById("tbody");
+        let tr = createElement("tr");
+        let tdFirstName = createElement("td",firstName);
+        let tdLastName = createElement("td",lastName);
+        let tdAddress = createElement("td",inputAddress1 + ", " + inputAddress2);
+        let tdPinccode = createElement("td",pincode);
+        let tdGender = createElement("td",gender);
+        let tdFood = createElement("td", food.join(","));
+        let tdState = createElement("td",state);
+        let tdCountry = createElement("td",country);
+        tr.append(tdFirstName,tdLastName,tdAddress,tdPinccode,tdGender,tdFood,tdState,tdCountry);
+        tbody.append(tr);
+        form.reset();
+    }
+    return false;
+}
+
+function createElement(elementName,value = ""){
+    var element = document.createElement(elementName);
+    element.innerHTML = value;
+    return element;
+}
